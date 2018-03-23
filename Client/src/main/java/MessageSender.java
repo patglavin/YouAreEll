5963843660c0a60cee4984d4dddbe09e9eac69be
Patrick Glavin <patjglavin@gmail.com>
2018-03-23 11:19:13 -0400

    extract component methods

--- Client/src/main/java/MessageSender.java
+++ Client/src/main/java/MessageSender.java
@@ -1,9 +1,11 @@
 import java.util.List;
 
 public class MessageSender implements Runnable{
-    List<String> list;
-    User user;
-    YouAreEll webber;
+    private List<String> list;
+    private User user;
+    private YouAreEll webber;
+    private Message message;
+    private String sentence = "";
 
     public MessageSender(List<String> list, User user, YouAreEll webber) {
         this.list = list;
@@ -12,25 +14,33 @@ public class MessageSender implements Runnable{
     }
 
     public void run() {
-        Message message;
-        String sentence = "";
-        for (int i = 2; i < list.size(); i++) {
-            sentence += list.get(i) + " ";
-        }
-
-        sentence = sentence.substring(0, sentence.length()-1);
-        if (list.get(1).equalsIgnoreCase("group")){
-            message = new Message(user.getGithub(), "", sentence);
-        } else {
-            message = new Message(user.getGithub(), list.get(1), sentence);
-        }
-
+        buildSentence();
+        buildMessage();
         String results = webber.send_message(message);
+        simulateLag();
+        SimpleShell.prettyPrintMessage(results);
+    }
+
+    private void simulateLag() {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e){
             e.printStackTrace();
         }
-        SimpleShell.prettyPrintMessage(results);
+    }
+
+    private void buildMessage() {
+        if (list.get(1).equalsIgnoreCase("group")){
+            message = new Message(user.getGithub(), "", sentence);
+        } else {
+            message = new Message(user.getGithub(), list.get(1), sentence);
+        }
+    }
+
+    private void buildSentence() {
+        for (int i = 2; i < list.size(); i++) {
+            sentence += list.get(i) + " ";
+        }
+        sentence = sentence.substring(0, sentence.length()-1);
     }
 }
